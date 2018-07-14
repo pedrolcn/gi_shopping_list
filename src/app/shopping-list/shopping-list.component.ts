50da70ebcb84f65770a5a18028bdc790077feeed
Pedro Nascimento <lobo@estudar.com.vc>
2018-07-14 16:55:27 -0300

    Feat: Add & edit list functionality

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,12 @@
 import { Item } from './../models/item';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['../../assets/shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, AfterViewChecked {
   itemList: Item[] = [new Item({ title: 'Test Item' })];
 
   constructor() { }
@@ -14,7 +14,12 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngAfterViewChecked() {
+    const lastItem = <HTMLElement>document.querySelector('app-shopping-item:last-of-type input[type="text"]');
+    lastItem.focus();
+  }
+
   add(): void {
-    this.itemList.push( new Item({ title: 'new item'}));
+    this.itemList.push( new Item({title: 'Novo item...' }) );
   }
 }
--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShoppingListComponent } from './shopping-list.component';
 import { ShoppingItemComponent } from './shopping-item.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
   ],
   declarations: [
     ShoppingListComponent,
-    ShoppingItemComponent
+    ShoppingItemComponent,
   ],
   exports: [
     ShoppingListComponent,
